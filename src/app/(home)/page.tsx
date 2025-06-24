cbaa5db2c33c8170faadf14394c7d9830568472c
limkyulee <mud0308@gmail.com>
2025-06-24 17:00:15 +0900

    trcp hydrateclient example

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,19 @@
-import { trpc } from "@/trpc/server";
+import { HydrateClient, trpc } from "@/trpc/server";
+import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 export default async function Home() {
 
-  const data = await trpc.hello({ text: "Kyuleelim" })
+  void trpc.hello.prefetch({ text: "Kyuleelim" })
 
   return (
-    <div>
-      Client component say : {data.greeting}
-    </div>
+    <HydrateClient>
+      <Suspense fallback={<p>Loading..</p>}>
+        <ErrorBoundary fallback={<p>Error..</p>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
   );
 }
