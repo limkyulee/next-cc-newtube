5c110ffe4c9f8a59068ebacdf26f7e0ec73df75b
limkyulee <mud0308@gmail.com>
2025-06-24 17:00:30 +0900

    trcp hydrateclient example

---
+++ src/app/(home)/client.tsx
@@ -0,0 +1,15 @@
+"use cilent"
+
+import { trpc } from "@/trpc/client"
+
+export const PageClient = () => {
+  const [data] = trpc.hello.useSuspenseQuery({
+    text: "Kyuleelim",
+  });
+
+  return (
+    <div>
+      Page Client says: {data.greeting}
+    </div>
+  )
+}
